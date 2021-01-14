5ea42984a5725d0c06be68b31e841a351882ff3d
Sulistio Sanjaya <56523212+SulistioSanjaya20@users.noreply.github.com>
2021-01-14 19:10:15 +0700

    Add project files.

---
+++ ServerChatApp_084_SulistioSanjaya/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_084;
+
+namespace ServerChatApp_084_SulistioSanjaya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(ServiceCallBack));
+                hostObjek.Open();
+
+                Console.WriteLine("Server On, Ready to use");
+                Console.ReadLine();
+
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
